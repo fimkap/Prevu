e5490caaf3beecb5c2ffb38f19076ddda17b8346
Efim Polevoi <fimkap@macbook-pro.local>
2016-11-20 18:45:54 +0100

    Initial Commit

---
+++ Prevu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Prevu
+//
+//  Created by Efim Polevoi on 20/11/2016.
+//  Copyright © 2016 Efim Polevoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
