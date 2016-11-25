bc21f5f3f9e5c2c6428415312a7dbcbae0f751d0
Efim Polevoi <fimkap@MacBook-Pro.local>
2016-11-25 02:34:37 +0100

    Terminate the previous app. Order front.

--- Prevu/AppDelegate.swift
+++ Prevu/AppDelegate.swift
@@ -16,11 +16,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        NSApplication.shared().activate(ignoringOtherApps: true)
+        let bundleID = Bundle.main.bundleIdentifier!
+        if NSRunningApplication.runningApplications(withBundleIdentifier: bundleID).count > 1 {
+            let apps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleID)
+            for app in apps {
+                if app != NSRunningApplication.current() {
+                    app.terminate()
+                }
+            }
+        }
+        //NSApplication.shared().activate(ignoringOtherApps: true)
+        NSApplication.shared().windows[0].orderFrontRegardless()
         NSApplication.shared().windows[0].isOpaque = false
         NSApplication.shared().windows[0].backgroundColor = NSColor.clear
         let args = CommandLine.arguments
-        print("Args: \(args)")
+        //print("Args: \(args)")
         if (args[1] != "-NSDocumentRevisionsDebugMode") {
             imagePath = args[1]
         }
